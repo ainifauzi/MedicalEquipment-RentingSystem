fb77dbf8b4aad8216ec1b323ec13f7d5b474f036
ainifzi <2022782581@student.uitm.edu.my>
2024-07-14 16:10:53 +0800

    equip model & controller readAll

--- app/Http/Controllers/EquipmentController.php
+++ app/Http/Controllers/EquipmentController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Models\Equipment;
 
 class EquipmentController extends Controller
 {
-    //
+    public function readAll()
+    {
+        $equipments = Equipment::all();
+        return response() -> json(array('data' => $equipments), 200);
+    }
 }
