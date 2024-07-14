4088822384a021e8ebf868efa53b76a567a3aed3
ainifzi <2022782581@student.uitm.edu.my>
2024-07-14 17:48:08 +0800

    model payment & controller readAll/ create

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Models\Payment;
+use App\Models\Application;
+
 
 class PaymentController extends Controller
 {
-    //
+    public function readAll()
+    {
+        $payments = Payment::all();
+        return response() -> json(array('data' => $payments), 200);
+    }
+
+    public function create(Request $request)
+    {
+        $createPayment = Payment::create([
+            'paymentId' => Str::uuid() -> toString(),
+            'paymentAmount' => $request -> input('paymentAmount'),
+            'paymentReceipt' => $request -> input('paymentReceipt')   
+        ]);
+
+        return response() -> json(array('data' => $createPayment), 200);
+    }
 }
--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        ' paymentId',
+        ' paymentAmount',
+        ' paymentReceipt',
+        ' applicationId'
+
+    ];
+
+    public $timestamps = false;
+    public $incrementing = false;
+    protected $keyType = 'string';
+    protected $primaryKey = 'paymentId';
 }
